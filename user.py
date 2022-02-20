1a3ceb8392b4c1ed8011b4e4d4c376219bb14ed4
Venus-Kavunza <venusmwende@gmail.com>
2022-02-20 23:34:23 +0300

    it will add a method that displays a list of credentials

--- user.py
+++ user.py
@@ -82,4 +82,14 @@ class Credential:
 		return user_credentials_list
 				
 
+	
+	@classmethod
+	def find_by_site_name(cls, site_name):
+		'''
+		This method takes in a site_name and returns a credential that matches that site_name.
+		'''
+		for credential in cls.credentials_list:
+			if credential.site_name == site_name:
+				return credential
 
+	
