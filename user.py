e3db4154bbab9754281f9f7f823e86d598e3e3b4
Venus-Kavunza <venusmwende@gmail.com>
2022-02-20 19:08:25 +0300

    it will add user class

---
+++ user.py
@@ -0,0 +1,12 @@
+import pyperclip
+import random
+import string
+
+global users_list
+class User:
+    """
+    This class creates user accounts and save their details
+    """
+
+    users_list[]
+    
