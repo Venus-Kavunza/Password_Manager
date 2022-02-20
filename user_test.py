634785d2dab6191a30dd27feffa4283776917181
Venus-Kavunza <venusmwende@gmail.com>
2022-02-20 23:40:24 +0300

    it will add a test for checking if the method returns correct credentals

--- user_test.py
+++ user_test.py
@@ -72,6 +72,16 @@ class TestCredentials(unittest.TestCase):
 		gmail.save_credentials()
 		self.assertEqual(len(Credential.display_credentials(twitter.user_name)),3)
 
+    def test_find_by_site_name(self):
+		'''
+		Test to check if the find_by_site_name method returns the correct credential
+		'''
+		self.new_credential.save_credentials()
+		twitter = Credential('Venus','Twitter','Vee','dwsp003')
+		twitter.save_credentials()
+		credential_exists = Credential.find_by_site_name('Twitter')
+		self.assertEqual(credential_exists,twitter)    
+
     
 if __name__ == '__main__':
 	unittest.main(verbosity=2)
