de15809e87275392936fb33e9fe24726c13eaa62
Venus-Kavunza <venusmwende@gmail.com>
2022-02-20 19:12:38 +0300

    it will add a method defining the properties of the user object

--- user.py
+++ user.py
@@ -9,4 +9,11 @@ class User:
     """
 
     users_list[]
-    
+    def __init__(self,first_name,last_name,password):
+        '''
+        This method defines the properties for each user object will hold.
+        '''
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.password = password
